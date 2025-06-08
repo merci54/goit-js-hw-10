b37b7fb937ecb57284ce49dc094a3ae3461f9583
Yaroslav <yaroslavlit@gmail.com>
2025-06-08 16:32:00 +0200

    add flatpickr

---
+++ src/js/1-timer.js
@@ -0,0 +1,18 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.querySelector('#datetime-picker');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    },
+};
+
+flatpickr(input, options)
